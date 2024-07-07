cc93455efef8ccef9c3d849f37597854c7de8cec
pritishm <mohantybetun@gmail.com>
2024-07-07 12:37:04 +0530

    added comment for Lab001.java

--- src/ex_07072024/Lab001.java
+++ src/ex_07072024/Lab001.java
@@ -1,9 +1,13 @@
 package ex_07072024;
 
 public class Lab001 {
+
+    //constructor
     public Lab001(){
 
     }
+
+    //main method
     public static void main(String[] args){
         System.out.println("Hello World");
     }
