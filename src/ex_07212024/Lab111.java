be813ad599de708e2467195015772fa007beb95c
pritishm <mohantybetun@gmail.com>
2024-07-21 18:33:50 +0530

    Added source code for date 21

---
+++ src/ex_07212024/Lab111.java
@@ -0,0 +1,9 @@
+package ex_07212024;
+
+public class Lab111 {
+    public static void main(String[] args) {
+        for (int i = 0; ; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/ex_07212024/Lab114.java
@@ -0,0 +1,10 @@
+package ex_07212024;
+
+public class Lab114 {
+    public static void main(String[] args) {
+        for (char c = 'a'; c <= 'z';c++){
+            System.out.println(c);
+        }
+
+    }
+}
