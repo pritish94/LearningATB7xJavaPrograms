6399274a671c21099a018c9b5d220cb197a82719
pritishm <mohantybetun@gmail.com>
2024-08-06 10:54:16 +0530

    updated code for assignment give on 27th July

---
+++ src/July/ex_07282024/AssignmentSecondLargestNumberInArray.java
@@ -0,0 +1,26 @@
+package July.ex_07282024;
+
+public class AssignmentSecondLargestNumberInArray {
+    public static void main(String[] args) {
+        int[] salaries = {7, 10, 4, 13, 25, 18, 9, 8, 17};
+
+//        Arrays.sort(salaries);
+//        System.out.printf("Second largest number in array is: %d",salaries[salaries.length-2]);
+
+        int largestNumber = Integer.MIN_VALUE;
+        int secondLargestNumber = Integer.MIN_VALUE;
+
+        for(int num: salaries){
+            if(num > largestNumber){
+                secondLargestNumber = largestNumber;
+                largestNumber = num;
+            } else if (num > secondLargestNumber && num!=largestNumber) {
+                secondLargestNumber = num;
+            }
+        }
+
+        System.out.printf("Second largest number in array is: %d",secondLargestNumber);
+
+
+    }
+}
